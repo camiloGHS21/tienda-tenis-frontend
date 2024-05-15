2f4d6daee73a0708684e0c1e7b7ae3eb19521b15
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-05-15 00:04:27 +0000

    subiendo pagina de  tienda de tenis

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,js}",
+  'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
+
