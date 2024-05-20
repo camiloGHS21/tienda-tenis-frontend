59455f999e60af7c544efadfd40c0b5e23761f39
nicolas grajales <nicolasgrajaleshoyos@Gmail.com>
2024-05-20 11:49:45 -0500

    creado login y registro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -111,6 +111,9 @@ export function Navbar() {
             <li>
               <Link to="/contact" className="block py-2 px-3 text-2xl text-black rounded md:bg-transparent " aria-current="page">Contact</Link>
             </li>
+            <li>
+            <Link to="/Login" className="block py-2 px-3 text-2xl text-black rounded md:bg-transparent " aria-current="page">Login</Link>
+            </li>
           </ul>
         </div>
 
