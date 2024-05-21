035be36dd28d76f337874315a72091a755ee57cc
nicolas grajales <nicolasgrajaleshoyos@Gmail.com>
2024-05-21 14:56:54 -0500

    update two of know us

--- src/pages/know_us.jsx
+++ src/pages/know_us.jsx
@@ -18,7 +18,7 @@ const AboutUs = () => {
           <ul className="mt-4 space-y-4 lg:space-y-0 lg:flex lg:flex-col lg:items-center lg:ml-4">
             <li className="flex items-center justify-center lg:justify-start">
               <div className="flex-shrink-0 h-10 w-10">
-                <img className="h-full w-full object-cover rounded-full" src="/public/img/descargar (1).jpg" alt="Michael Jordan" />
+                <img className="h-full w-full object-cover rounded-full" src="/img/descargar (1).jpg" alt="Michael Jordan" />
               </div>
               <div className="ml-4">
                 <p className="text-sm font-medium text-gray-900">Michael Jordan</p>
@@ -27,7 +27,7 @@ const AboutUs = () => {
             </li>
             <li className="flex items-center justify-center lg:justify-start">
               <div className="flex-shrink-0 h-10 w-10">
-                <img className="h-full w-full object-cover rounded-full" src="/public/img/130097149.jpg" alt="Nicolas Grajales" />
+                <img className="h-full w-full object-cover rounded-full" src="/img/130097149.jpg" alt="Nicolas Grajales" />
               </div>
               <div className="ml-4">
                 <p className="text-sm font-medium text-gray-900">Nicolas Grajales hoyos</p>
@@ -36,7 +36,7 @@ const AboutUs = () => {
             </li>
             <li className="flex items-center justify-center lg:justify-start">
               <div className="flex-shrink-0 h-10 w-10">
-                <img className="h-full w-full object-cover rounded-full" src="/public/img/camilo (1).png" alt="Camilo Grajales" />
+                <img className="h-full w-full object-cover rounded-full" src="/img/camilo (1).png" alt="Camilo Grajales" />
               </div>
               <div className="ml-4">
                 <p className="text-sm font-medium text-gray-900">Camilo Grajales Holgin</p>
