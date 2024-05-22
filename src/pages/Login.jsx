75b3a557864d40d7755ab3944399472f392ea84b
camiloghs21 <camiloghs21@gmail.com>
2024-05-22 07:33:24 -0700

    organizando boton del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,7 +41,6 @@ export default function Login() {
                                 </div>
                                 
                             </div>
-                            <button type="submit" className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Sign up</button>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="/Register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
