cd58e3f30b87cc49533547883039a4655310deb5
nicolas grajales <nicolasgrajaleshoyos@Gmail.com>
2024-05-22 11:28:39 -0500

    cuadrando footer de la pagina

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <footer className="bg-gray-100 rounded-lg shadow m-4 dark:bg-gray-900">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Jordan.CN™</a>. All Rights Reserved.
                 </span>
